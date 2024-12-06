be4ce2d6fcb645cf11d16280631c988576be4fdc
Narayandwivedi <desinplus1@gmail.com>
2024-12-06 20:40:40 +0530

    some random changes

--- src/Landing_page/home/Awards.jsx
+++ src/Landing_page/home/Awards.jsx
@@ -1,50 +1,3 @@
-<<<<<<< Updated upstream
-
-
-function Awards() {
-  return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-6 p-5">
-          <img src="image/largestBroker.svg" />
-        </div>
-        <div className="col-6 p-5 mt-5">
-          <h1>Largest stock broker in India</h1>
-          <p className="mb-5">
-            2+ million Zerodha clients contribute to over 15% of all retail
-            order volumes in India daily by trading and investing in:
-          </p>
-          <div className="row">
-            <div className="col-6">
-              <ul>
-                <li>
-                  <p>Futures and Options</p>
-                </li>
-                <li>
-                  <p>Commodity derivatives</p>
-                </li>
-                <li>
-                  <p>Currency derivatives</p>
-                </li>
-              </ul>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>
-                  <p>Stocks & IPOs</p>
-                </li>
-                <li>
-                  <p>Direct mutual funds</p>
-                </li>
-                <li>
-                  <p>Bonds and Govt. Securities</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <img src="image/pressLogos.png" style={{ width: "90%" }} />
-        </div>
-=======
 export default function Awards() {
   return (
     <div className="container mt-5">
@@ -62,12 +15,7 @@ export default function Awards() {
 
         <div className="row">
             <ul className="col">
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-            <ul className="col">
-                <li>1</li>
+                <li>Futures and op</li>
                 <li>2</li>
                 <li>3</li>
             </ul>
@@ -76,13 +24,7 @@ export default function Awards() {
 
         </div>
 
->>>>>>> Stashed changes
       </div>
     </div>
   );
 }
-<<<<<<< Updated upstream
-
-export default Awards;
-=======
->>>>>>> Stashed changes
