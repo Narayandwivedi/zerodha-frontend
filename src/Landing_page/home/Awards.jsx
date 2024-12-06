306a7f232aa752eaf37fa61682df451c939d9a60
Narayandwivedi <desinplus1@gmail.com>
2024-12-06 08:35:37 +0530

    fix conflit

--- src/Landing_page/home/Awards.jsx
+++ src/Landing_page/home/Awards.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 
 function Awards() {
@@ -43,9 +44,45 @@ function Awards() {
           </div>
           <img src="image/pressLogos.png" style={{ width: "90%" }} />
         </div>
+=======
+export default function Awards() {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+
+        <div className="col">
+          <img src="image/largestBroker.svg" alt="" />
+        </div>
+
+        <div className="col">
+          <h1>Largest stock broker in India</h1>
+          <p className="fs-5">2+ million Zerodha contribute to over 15% of all retail order
+            volumes in indiadaily by trading  and investing in:
+          </p>
+
+        <div className="row">
+            <ul className="col">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+            <ul className="col">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+
+
+        </div>
+
+>>>>>>> Stashed changes
       </div>
     </div>
   );
 }
+<<<<<<< Updated upstream
 
 export default Awards;
+=======
+>>>>>>> Stashed changes
--- src/Landing_page/home/Hero.jsx
+++ src/Landing_page/home/Hero.jsx
@@ -1,6 +1,7 @@
 
 function Hero() {
   return (
+<<<<<<< Updated upstream
     <div className="container p-5 mb-5">
       <div className="row text-center">
         <img
@@ -19,6 +20,17 @@ function Hero() {
         >
           Signup Now
         </button>
+=======
+    <div className="container">
+      <div className="row p-5 text-center" >
+        <img className="mb-5" src="image/homeHero.png" alt="hero image" />
+        <h1 className="mt-4">Invest in everything</h1>
+        <p className="fs-5 m-2">
+          Online platform to invest in stocks, derivatives, mutual funds, ETFs,
+          bonds, and more.
+        </p>
+        <button className="p-2 mb-5 mt-4 btn btn-primary fs-5" style={{width : "22%" , margin : "0 auto"}}>sign now </button>
+>>>>>>> Stashed changes
       </div>
     </div>
   );
